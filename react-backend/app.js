c0d58720f3b3dc24a05c632e9bd146578d93989a
Austin Tackaberry <austintackaberry@gmail.com>
2017-12-10 15:54:11 -0800

    20s -> 3s -> :)

--- react-backend/app.js
+++ react-backend/app.js
@@ -79,202 +79,240 @@ app.post('/getresults', function(req, res) {
   var returnDataPackage = [];
   var stackOverflowFormatted = [];
   var asyncFns = [];
-  async.series([
-    (callback) => {
-      if (dataPackage.checked.github) {
-        var githubData = {};
-        var url = "https://jobs.github.com/positions.json?search=" + dataPackage.jobTitle + '&location=' + dataPackage.jobLocation;
-        fetch(url, {
-          method: 'GET'
-        })
-        .then(res => res.json())
-        .catch(e => {
-          console.log(e);
-        })
-        .then(data => {
-          githubData = data;
-          var githubFormatted = [];
-          for (let j = 0; j < githubData.length; j++) {
-            githubFormatted.push(
-              {
-                url: 'https://jobs.github.com/positions/' + githubData[j].id,
-                title: githubData[j].title,
-                postTime: githubData[j].created_at,
-                location: githubData[j].location,
-                type: githubData[j].type,
-                description: githubData[j].description,
-                source: "github",
-                rankScore: rankScore(dataPackage, githubData[j].description)
-              }
-            );
-          }
-          returnDataPackage = returnDataPackage.concat(githubFormatted);
-          callback();
-        })
-        .catch(e => {
-          console.log(e);
-        });
-      }
-    },
-    (callback) => {
-      if (dataPackage.checked.hackerNews) {
-        const options = {
-          uri: 'https://news.ycombinator.com/submitted?id=whoishiring',
-          transform: (body) => {return cheerio.load(body);}
-        };
-        rp(options)
-        .then(($) => {
-          let whoIsHiringLink;
-          var hnFormatted = [];
-          $('.storylink').each(function(index, value) {
-            let text = $(this).text();
-            if (text.includes('Who is hiring?')) {
-              whoIsHiringLink = 'https://news.ycombinator.com/' + value.attribs.href;
-              return false;
+  var asyncFns2 = [];
+  if (dataPackage.checked.github) {
+    console.log('github');
+    asyncFns.push(
+      (() => {
+        return new Promise((resolve, reject) => {
+          var githubData = {};
+          var url = "https://jobs.github.com/positions.json?search=" + dataPackage.jobTitle + '&location=' + dataPackage.jobLocation;
+          fetch(url, {
+            method: 'GET'
+          })
+          .then(res => res.json())
+          .catch(e => {
+            console.log(e);
+          })
+          .then(data => {
+            githubData = data;
+            var githubFormatted = [];
+            for (let j = 0; j < githubData.length; j++) {
+              githubFormatted.push(
+                {
+                  url: 'https://jobs.github.com/positions/' + githubData[j].id,
+                  title: githubData[j].title,
+                  postTime: githubData[j].created_at,
+                  location: githubData[j].location,
+                  type: githubData[j].type,
+                  description: githubData[j].description,
+                  source: "github",
+                  rankScore: rankScore(dataPackage, githubData[j].description)
+                }
+              );
             }
+            returnDataPackage = returnDataPackage.concat(githubFormatted);
+            resolve();
+          })
+          .catch(e => {
+            console.log(e);
           });
+        });
+      })()
+    );
+  }
+  if (dataPackage.checked.hackerNews) {
+    console.log('hackern');
+    asyncFns.push(
+      (() => {
+        return new Promise((resolve, reject) => {
           const options = {
-            uri: whoIsHiringLink,
+            uri: 'https://news.ycombinator.com/submitted?id=whoishiring',
             transform: (body) => {return cheerio.load(body);}
           };
           rp(options)
           .then(($) => {
-            $('.c00').each(function(index, value) {
+            let whoIsHiringLink;
+            var hnFormatted = [];
+            $('.storylink').each(function(index, value) {
               let text = $(this).text();
-              let topLine = $($(this).contents()[0]).text();
-              let listingInfo = topLine.split("|");
-              if (listingInfo.length > 1) {
-                let i = 0;
-                let j = 0;
-                let descriptionHTML;
-                let fullPost = $(this).html();
-                hnFormatted.push({source:"hackerNews", fullPostText:text, fullPostHTML:fullPost})
-                if ($($(this).contents()[1]).attr('href')) {
-                  hnFormatted[hnFormatted.length -1].url = $($(this).contents()[1]).attr('href');
-                  descriptionHTML = $($(this).contents().slice(2)).text();
-                }
-                else {
-                  descriptionHTML = $($(this).contents().slice(1)).text();
-                }
-                hnFormatted[hnFormatted.length -1].companyName = listingInfo.shift();
-                hnFormatted[hnFormatted.length -1].description = descriptionHTML;
-                hnFormatted[hnFormatted.length - 1].rankScore = rankScore(dataPackage, text);
-                while (i < listingInfo.length) {
-                  if (listingInfo[i].includes('http')) {
-                    hnFormatted[hnFormatted.length -1].url = listingInfo.splice(i, 1);
-                  }
-                  else if (/%|salary|€|\$|£|[0-9][0-9]k/.test(listingInfo[i])) {
-                    hnFormatted[hnFormatted.length -1].compensation = listingInfo.splice(i, 1)[0];
-                  }
-                  else if (/position|engineer|developer|senior|junior|scientist|analyst/i.test(listingInfo[i])) {
-                    hnFormatted[hnFormatted.length -1].title = listingInfo.splice(i, 1)[0];
-                  }
-                  else if (/permanent|intern|flexible|remote|on\W*site|part\Wtime|full\Wtime|full/i.test(listingInfo[i])) {
-                    hnFormatted[hnFormatted.length -1].type = listingInfo.splice(i, 1)[0];
-                  }
-                  else if (/boston|seattle|london|new york|san francisco|bay area|nyc|sf/i.test(listingInfo[i])) {
-                    hnFormatted[hnFormatted.length -1].location = listingInfo.splice(i, 1)[0];
-                  }
-                  else if (/\W\W[A-Z][a-zA-Z]/.test(listingInfo[i])) {
-                    hnFormatted[hnFormatted.length -1].location = listingInfo.splice(i, 1)[0];
-                  }
-                  else if (/[a-z]\.[a-z]/i.test(listingInfo[i])) {
-                    hnFormatted[hnFormatted.length -1].url = "http://" + listingInfo.splice(i, 1)[0];
-                  }
-                  else if (listingInfo[i] === " ") {
-                    listingInfo.splice(i, 1);
-                  }
-                  else {
-                    i++;
-                  }
-                  j++;
-                }
-                // if (listingInfo.length > 0) {
-                //   console.log(listingInfo);
-                // }
+              if (text.includes('Who is hiring?')) {
+                whoIsHiringLink = 'https://news.ycombinator.com/' + value.attribs.href;
+                return false;
               }
             });
-            returnDataPackage = returnDataPackage.concat(hnFormatted);
-            callback();
+            const options = {
+              uri: whoIsHiringLink,
+              transform: (body) => {return cheerio.load(body);}
+            };
+            rp(options)
+            .then(($) => {
+              $('.c00').each(function(index, value) {
+                let text = $(this).text();
+                let topLine = $($(this).contents()[0]).text();
+                let listingInfo = topLine.split("|");
+                if (listingInfo.length > 1) {
+                  let i = 0;
+                  let j = 0;
+                  let descriptionHTML;
+                  let fullPost = $(this).html();
+                  hnFormatted.push({source:"hackerNews", fullPostText:text, fullPostHTML:fullPost})
+                  if ($($(this).contents()[1]).attr('href')) {
+                    hnFormatted[hnFormatted.length -1].url = $($(this).contents()[1]).attr('href');
+                    descriptionHTML = $($(this).contents().slice(2)).text();
+                  }
+                  else {
+                    descriptionHTML = $($(this).contents().slice(1)).text();
+                  }
+                  hnFormatted[hnFormatted.length -1].companyName = listingInfo.shift();
+                  hnFormatted[hnFormatted.length -1].description = descriptionHTML;
+                  hnFormatted[hnFormatted.length - 1].rankScore = rankScore(dataPackage, text);
+                  while (i < listingInfo.length) {
+                    if (listingInfo[i].includes('http')) {
+                      hnFormatted[hnFormatted.length -1].url = listingInfo.splice(i, 1);
+                    }
+                    else if (/%|salary|€|\$|£|[0-9][0-9]k/.test(listingInfo[i])) {
+                      hnFormatted[hnFormatted.length -1].compensation = listingInfo.splice(i, 1)[0];
+                    }
+                    else if (/position|engineer|developer|senior|junior|scientist|analyst/i.test(listingInfo[i])) {
+                      hnFormatted[hnFormatted.length -1].title = listingInfo.splice(i, 1)[0];
+                    }
+                    else if (/permanent|intern|flexible|remote|on\W*site|part\Wtime|full\Wtime|full/i.test(listingInfo[i])) {
+                      hnFormatted[hnFormatted.length -1].type = listingInfo.splice(i, 1)[0];
+                    }
+                    else if (/boston|seattle|london|new york|san francisco|bay area|nyc|sf/i.test(listingInfo[i])) {
+                      hnFormatted[hnFormatted.length -1].location = listingInfo.splice(i, 1)[0];
+                    }
+                    else if (/\W\W[A-Z][a-zA-Z]/.test(listingInfo[i])) {
+                      hnFormatted[hnFormatted.length -1].location = listingInfo.splice(i, 1)[0];
+                    }
+                    else if (/[a-z]\.[a-z]/i.test(listingInfo[i])) {
+                      hnFormatted[hnFormatted.length -1].url = "http://" + listingInfo.splice(i, 1)[0];
+                    }
+                    else if (listingInfo[i] === " ") {
+                      listingInfo.splice(i, 1);
+                    }
+                    else {
+                      i++;
+                    }
+                    j++;
+                  }
+                  // if (listingInfo.length > 0) {
+                  //   console.log(listingInfo);
+                  // }
+                }
+              });
+              returnDataPackage = returnDataPackage.concat(hnFormatted);
+              resolve();
+            })
+            .catch((err) => {
+              console.log(err);
+            });
           })
           .catch((err) => {
             console.log(err);
           });
-        })
-        .catch((err) => {
-          console.log(err);
         });
-      }
-    },
-    (callback) => {
-      if (dataPackage.checked.stackOverflow) {
-        const options = {
-          uri: 'https://stackoverflow.com/jobs?q=' + dataPackage.jobTitle + '&l=' + dataPackage.jobLocation + '&d=20&u=Miles&sort=i',
-          transform: (body) => {return cheerio.load(body);}
-        };
-        rp(options)
-        .then(($) => {
-          $('.-job-item').each(function(index, value) {
-            asyncFns.push(
-              () => {
-                return new Promise((resolve, reject) => {
-                  stackOverflowFormatted.push({});
-                  let postTime = $(this).find('.-posted-date.g-col').text();
-                  stackOverflowFormatted[index].postTime = postTime.trim();
-                  var url = 'https://stackoverflow.com/jobs/' + $(this).attr('data-jobid');
-                  const options = {
-                    uri: url,
-                    transform: (body) => {return cheerio.load(body);}
-                  };
-                  rp(options)
-                  .then(($) => {
-                    stackOverflowFormatted[index].title = $('a.title.job-link').attr('title');
-                    stackOverflowFormatted[index].companyName = $('a.employer').text();
-                    stackOverflowFormatted[index].location = $('div.-location').first().text().trim().replace("- \n","");
-                    stackOverflowFormatted[index].url = url;
-                    stackOverflowFormatted[index].source = "stackOverflow";
-                    stackOverflowFormatted[index].description = $('div.description').text();
-                    stackOverflowFormatted[index].rankScore = rankScore(dataPackage, stackOverflowFormatted[index].description);
-                    resolve();
-                  })
-                  .catch((err) => {
-                    console.log(err);
+      })()
+    );
+  }
+  if (dataPackage.checked.stackOverflow) {
+    console.log('stacko');
+    asyncFns.push(
+      (() => {
+        return new Promise((resolve1, reject) => {
+          const options = {
+            uri: 'https://stackoverflow.com/jobs?q=' + dataPackage.jobTitle + '&l=' + dataPackage.jobLocation + '&d=20&u=Miles&sort=i',
+            transform: (body) => {return cheerio.load(body);}
+          };
+          rp(options)
+          .then(($) => {
+            $('.-job-item').each(function(index, value) {
+              asyncFns2.push(
+                ((index) => {
+                  return new Promise((resolve, reject) => {
+                    stackOverflowFormatted.push({});
+                    let postTime = $(this).find('.-posted-date.g-col').text();
+                    stackOverflowFormatted[index].postTime = postTime.trim();
+                    var url = 'https://stackoverflow.com/jobs/' + $(this).attr('data-jobid');
+                    const options = {
+                      uri: url,
+                      transform: (body) => {return cheerio.load(body);}
+                    };
+                    rp(options)
+                    .then(($) => {
+                      stackOverflowFormatted[index].title = $('a.title.job-link').attr('title');
+                      stackOverflowFormatted[index].companyName = $('a.employer').text();
+                      stackOverflowFormatted[index].location = $('div.-location').first().text().trim().replace("- \n","");
+                      stackOverflowFormatted[index].url = url;
+                      stackOverflowFormatted[index].source = "stackOverflow";
+                      stackOverflowFormatted[index].description = $('div.description').text();
+                      stackOverflowFormatted[index].rankScore = rankScore(dataPackage, stackOverflowFormatted[index].description);
+                      console.log('way too long');
+                      resolve();
+                    })
+                    .catch((err) => {
+                      console.log(err);
+                    });
                   });
+                })(index)
+              );
+            });
+            asyncFns2.push(
+              (() => {
+                return new Promise((resolve, reject) => {
+                  returnDataPackage = returnDataPackage.concat(stackOverflowFormatted)
+                  returnDataPackage = jobSort(returnDataPackage);
+                  resolve();
                 });
-              }
+              })()
             );
+            resolve1();
+            // Promise.all(asyncFns2).then(() => {
+            // })
+            // .catch((err) => {
+            //   console.log(err);
+            // });
+            // for (var i = 0; i < asyncFns2.length; i++) {
+            //   promise = promise.then(asyncFns2[i]);
+            // }
+            // asyncFns = asyncFns.concat(asyncFns2);
+          })
+          .catch((err) => {
+            console.log(err);
           });
-          asyncFns.push(
-            () => {
-              return new Promise((resolve, reject) => {
-                returnDataPackage = returnDataPackage.concat(stackOverflowFormatted)
-                returnDataPackage = jobSort(returnDataPackage);
-                resolve();
-              });
-            }
-          );
-          callback();
-        })
-        .catch((err) => {
-          console.log(err);
         });
-      }
-    },
-    (callback) => {
-      asyncFns.push(
-        () => {
-          return new Promise((resolve, reject) => {
-            res.send(returnDataPackage);
-            resolve();
-          });
-        }
-      );
-      var promise = asyncFns[0]();
-      for (var i = 1; i < asyncFns.length; i++) {
-        promise = promise.then(asyncFns[i]);
-      }
-    }
-  ]);
+      })()
+    );
+  }
+  // if (!dataPackage.checked.stackOverflow) {
+  //   asyncFns.push(
+  //     () => {
+  //       return new Promise((resolve, reject) => {
+  //         returnDataPackage = jobSort(returnDataPackage);
+  //         res.send(returnDataPackage);
+  //         resolve();
+  //       });
+  //     }
+  //   );
+  // }
+  // var promise = asyncFns[0]();
+  // for (var i = 1; i < asyncFns.length; i++) {
+  //   promise = promise.then(asyncFns[i]);
+  // }
+  console.log(asyncFns.length);
+  Promise.all(asyncFns).then(() => {
+    Promise.all(asyncFns2).then(() => {
+      returnDataPackage = jobSort(returnDataPackage);
+      res.send(returnDataPackage);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 });
 
 app.listen(process.env.PORT || 3001);
